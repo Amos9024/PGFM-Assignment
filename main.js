9cd0ae41751a6735fd8adba246cb2e5063cc275e
Amos9024 <hoamos90@gmail.com>
2025-05-12 12:07:24 +0800

    Improve web application with css styles and additional functions.

--- main.js
+++ main.js
@@ -5,19 +5,31 @@ console.log("Test test");
 // 1. Identify the DOM element to update
 const foodImage = document.getElementById("foodImage");
 const generateFoodBtn = document.getElementById("generateRecipe");
+const learnMore = document.getElementById("learnMore");
+const foodName = document.getElementById("foodName");
+const foodProtein = document.getElementById("Protein");
+const foodFat = document.getElementById("Fat");
+const foodCarbs = document.getElementById("Carbs");
 
-// 2. call the API
+// 2. Call the API with the function
 const requestOptions = {
     method: "GET"
 }
 function generateRecipe(){
     console.log("hi")
-    fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey1}&number=1&includeNutrition=true&include-tags=vegetarian, dessert`, requestOptions)
+    fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey1}&number=1&includeNutrition=true&include-tags=vegetarian`, requestOptions)
     .then((response) => response.json())
-    .then((result) => foodImage.src = result.recipes[0].image)
+    .then((result) => {
+        foodImage.src = result.recipes[0].image; 
+        learnMore.setAttribute("href",result.recipes[0].sourceUrl) 
+        foodName.innerHTML = result.recipes[0].title;
+        foodProtein.innerHTML = result.recipes[0].nutrition.caloricBreakdown.percentProtein;
+        foodFat.innerHTML = result.recipes[0].nutrition.caloricBreakdown.percentFat;
+        foodCarbs = result.recipes[0].nutrition.caloricBreakdown.percentCarbs;
+
+    })
     .catch((error) => console.error(error));
 }
-
 
 // 3. add in event listener to the buton
 generateFoodBtn.addEventListener("click",generateRecipe);
